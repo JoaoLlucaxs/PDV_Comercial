eaa39d7c90b3a464ceab7a4c91266091b2d49fe1
devjoao <joaolucasqueiriz@gmail.com>
2023-11-24 01:17:00 -0300

    tratando exceptions

---
+++ src/main/java/com/pdv/jsf/util/jsfExceptionHandlerFactory.java
@@ -0,0 +1,19 @@
+package com.pdv.jsf.util;
+
+import javax.faces.context.ExceptionHandler;
+import javax.faces.context.ExceptionHandlerFactory;
+
+public class jsfExceptionHandlerFactory extends ExceptionHandlerFactory{
+
+	private ExceptionHandlerFactory parent;
+	
+	public jsfExceptionHandlerFactory(ExceptionHandlerFactory parent) {
+		this.parent=parent;
+	}
+	
+	@Override
+	public ExceptionHandler getExceptionHandler() {
+		return new jsfExceptionHandler(parent.getExceptionHandler());
+	}
+
+}
