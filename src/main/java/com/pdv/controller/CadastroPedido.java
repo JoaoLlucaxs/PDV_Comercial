a9ce41c2e208e916f474c7d16fa1b6d28e7d8890
JoaoLlucaxs <queirozjoaolucas8@gmail.com>
2023-12-19 15:35:59 -0300

    corrigindo erros de telas modificadas

--- src/main/java/com/pdv/controller/CadastroPedido.java
+++ src/main/java/com/pdv/controller/CadastroPedido.java
@@ -3,13 +3,12 @@ package com.pdv.controller;
 import javax.faces.bean.RequestScoped;
 import javax.inject.Named;
 
-import com.pdv.service.NegocioException;
 
 @Named
 @RequestScoped
 public class CadastroPedido {
 	
 	public void salva() {
-		throw new NegocioException("Não há como cadastrar pedido");
+
 	}
 }
