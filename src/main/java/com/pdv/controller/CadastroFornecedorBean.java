b5515803ace1141dfee400e72389b5b4e53ae384
JoaoLlucaxs <queirozjoaolucas8@gmail.com>
2024-01-24 15:06:38 -0300

    Novas telas, mudanças em alguns detalhes

---
+++ src/main/java/com/pdv/controller/CadastroFornecedorBean.java
@@ -0,0 +1,14 @@
+package com.pdv.controller;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Named
+@ViewScoped
+public class CadastroFornecedorBean implements Serializable {
+    private static final long serialVersionUID=1L;
+
+}
